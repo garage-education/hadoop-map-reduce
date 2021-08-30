5136cf59df17e379f64b5f05a7ff453757019d2f
Moustafa Alaa Mohamed Mahmoud <moustafa.alaa@onfido.com>
2021-08-30 14:58:26 +0100

    add initial code

---
+++ src/main/java/mr01/SimpleWordCount.java
@@ -0,0 +1,69 @@
+package mr01;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class SimpleWordCount {
+
+    public static class MapperSide extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private final Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            //replace all special chars
+            //String pattern = "[-+.^:,()\"'/&*!;\\[\\]\\?$#]";
+
+            //simple text transformation upperCase then remove special chars.
+            //String line = value.toString().toUpperCase().replaceAll(pattern, "");
+
+            StringTokenizer itr = new StringTokenizer(value.toString());
+
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class ReducerSide extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if(args.length != 2){
+            System.err.println("Usage WordCount <input_file> <output_dir>");
+            System.exit(2);
+        }
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(SimpleWordCount.class);
+        job.setMapperClass(MapperSide.class);
+        job.setCombinerClass(ReducerSide.class);
+        job.setReducerClass(ReducerSide.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
