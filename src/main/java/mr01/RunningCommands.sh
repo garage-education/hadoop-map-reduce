e30203febfc297e30970094e48d2eb6325c9a373
Moustafa Mahmoud <mosalaa@amazon.com>
2021-09-26 12:08:44 +0100

    update git ignore and code

---
+++ src/main/java/mr01/RunningCommands.sh
@@ -0,0 +1,7 @@
+JAR location : s3://bucket/hadoop-map-reduce-1.0-SNAPSHOT-Without-Combiner.jar
+Main class : None
+Arguments : s3://bucket/shakespeare2.txt s3://bucket/OutputWithoutCombiner/
+
+JAR location : s3://bucket/hadoop-map-reduce-1.0-SNAPSHOT-Combiner.jar
+Main class : None
+Arguments : s3://bucket/shakespeare2.txt s3://bucket/OutputWithCombiner/
