61bbaa5ce27d5143aef9378aa601b399591f5805
Moustafa Mahmoud <mosalaa@amazon.com>
2021-11-17 00:51:29 +0000

    add joins

---
+++ src/main/java/mr08/map_side_join/DriverReplicatedJoin.java
@@ -0,0 +1,82 @@
+package mr08.map_side_join;
+
+import mr06.secondary_sort.SecondarySortDriver;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+
+public class DriverReplicatedJoin extends Configured implements Tool {
+    private static Logger logger = Logger.getLogger(DriverReplicatedJoin.class);
+
+    private static final String DATA_SEPARATOR = ",";
+
+    public int run(String[] args) throws Exception {
+        String inputTransactionData = args[0];
+        String inputLookupData = args[1];
+        String output = args[2];
+        Configuration conf = new Configuration();
+        conf.set("join.type", "inner");
+        conf.set("lkp.file.path", inputLookupData);
+        conf.set("mapreduce.output.textoutputformat.separator", DATA_SEPARATOR);
+
+        FileSystem fs = FileSystem.get(conf);
+        boolean exists = fs.exists(new Path(output));
+        if (exists) {
+            fs.delete(new Path(output), true);
+        }
+        Job job = Job.getInstance(conf);
+        job.setJarByClass(DriverReplicatedJoin.class);
+        job.setJobName("Replicated Map Join Join");
+        FileInputFormat.addInputPath(job, new Path(inputTransactionData));
+        FileOutputFormat.setOutputPath(job, new Path(output));
+        job.setMapperClass(MapperReplicatedJoin.class);
+        job.setNumReduceTasks(0);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        boolean status = job.waitForCompletion(true);
+        logger.info("run(): status="+status);
+        return status ? 0 : 1;
+    }
+
+    /**
+     * The main driver for word count map/reduce program.
+     * Invoke this method to submit the map/reduce job.
+     *
+     * @throws Exception When there is communication problems with the job tracker.
+     */
+    public static void main(String[] args) throws Exception {
+        // Make sure there are exactly 2 parameters
+        if (args.length != 3) {
+            logger.warn("DriverReplicatedJoin <input-trnx-dir> <input-lkp-dir> <output-dir>");
+            throw new IllegalArgumentException("DriverReplicatedJoin <input-trnx-dir> <input-lkp-dir> <output-dir>");
+        }
+
+
+        int returnStatus = submitJob(args);
+        logger.info("returnStatus=" + returnStatus);
+
+        System.exit(returnStatus);
+    }
+
+
+    /**
+     * The main driver for word count map/reduce program.
+     * Invoke this method to submit the map/reduce job.
+     *
+     * @throws Exception When there is communication problems with the job tracker.
+     */
+    public static int submitJob(String[] args) throws Exception {
+
+        int returnStatus = ToolRunner.run(new SecondarySortDriver(), args);
+        return returnStatus;
+    }
+}
---
+++ src/main/java/mr08/map_side_join/MapperReplicatedJoin.java
@@ -0,0 +1,58 @@
+package mr08.map_side_join;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+//Phases lifecycle are setup -> map -> cleanup
+//setup -> reduce -> cleanup
+//https://stackoverflow.com/questions/25432598/what-is-the-mapper-of-reducer-setup-used-for/25450627
+//https://lintool.github.io/MapReduceAlgorithms/MapReduce-book-final.pdf
+//https://lintool.github.io/MapReduceAlgorithms/ed1.html
+public class MapperReplicatedJoin extends Mapper<Object, Text, Text, Text> {
+    private static final Text EMPTY_TEXT = new Text("");
+    private HashMap<String, String> serviceid_To_pincode = new HashMap<String, String>();
+    private Text outvalue = new Text();
+    private String joinType = null;
+    private String service_id_file_location = null;
+
+    @Override
+    public void map(Object key, Text values, Context context) throws IOException, InterruptedException {
+        String data = values.toString();
+        String[] field = data.split(",", -1);
+        String service_id = field[0];
+        String pincode = serviceid_To_pincode.get(service_id);
+        outvalue.set(EMPTY_TEXT);
+        if (null != pincode && pincode.length() > 0) {
+            outvalue.set(pincode);
+            context.write(values, outvalue);
+        } else if (joinType.equalsIgnoreCase("leftouter")) {
+            context.write(values, EMPTY_TEXT);
+        }
+    }
+
+    @Override
+    public void setup(Context context) {
+        joinType = context.getConfiguration().get("join.type");
+        service_id_file_location = context.getConfiguration().get("service.id.file.path");
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(service_id_file_location));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] field = line.split(",", -1);
+                String service_id = field[0];
+                String pincode = field[1];
+                if (null != service_id && service_id.length() > 0 && null != pincode && pincode.length() > 0) {
+                    serviceid_To_pincode.put(service_id, pincode);
+                }
+            }
+            bufferedReader.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getLocalizedMessage());
+        }
+    }
+}
